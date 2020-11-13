45652345b47154d9f305b6e9fbcfcd616104a31d
rogerferdinan <rogerferdian@gmail.com>
2020-11-13 08:52:58 +0700

    visual update

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,6 +1,13 @@
 function DropDownContent(x) {
 	var y = x.nextElementSibling;
+	var a = x.querySelector("a > #content-triangle");
 	y.classList.toggle("show");
+    if (a.className === "content-triangle-down") {
+		a.className = "content-triangle-up";
+	}
+	else {
+		a.className = "content-triangle-down";
+	}
 }
 
 
