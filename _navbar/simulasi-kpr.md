c6168d46f06bf90dbedeb7aaee28632187726536
rogerferdinan <rogerferdian@gmail.com>
2020-11-13 15:52:58 +0700

    final

--- _navbar/simulasi-kpr.md
+++ _navbar/simulasi-kpr.md
@@ -13,12 +13,12 @@ js_file:
         <input autocomplete="off" type="text" id="harga_rumah" placeholder="Harga properti">
     </div>
     <div class="form_wrapper">
-        <label for="uang_muka">Uang Muka / DP 20%</label>
-        <input type="text" id="uang_muka" value="0" readonly>
+        <label for="uang_muka">Uang Muka</label>
+        <input type="text" id="uang_muka" value="Rp 0">
     </div>
     <div class="form_wrapper">
         <label for="pokok_kredit">Pokok Kredit</label>
-        <input type="text" id="pokok_kredit" value="0" readonly>
+        <input type="text" id="pokok_kredit" value="Rp 0" readonly>
     </div>
     <div class="form_wrapper">
         <label for="tenor">Jangka Waktu</label>
@@ -34,14 +34,14 @@ js_file:
     <div class="form_wrapper">
         <label for="suku_bunga">Estimasi Suku Bunga</label>
         <select id="suku_bunga">
-            {% for i in (6..14) %}
+            {% for i in (5..14) %}
                 <option value="{{ i }}"> {{ i }}% </option>
             {% endfor %}
         </select>
     </div>
     <div class="form_wrapper">
         <label for="angsuran">Angsuran</label>
-        <input type="text" id="angsuran" value="0" readonly>
+        <input type="text" id="angsuran" value="Rp 0" readonly>
     </div>
 </form>
-<div>Note : Belum termasuk biaya proses KPR, biaya notaris, pajak, dan lainnya.</div>
+<div class="div">Note : Belum termasuk biaya proses KPR, biaya notaris, pajak, dan lainnya.</div>
--- assets/js/simulasi-kpr.js
+++ assets/js/simulasi-kpr.js
@@ -1,24 +1,41 @@
 function simulasi_kpr() {
     var Currency = {currency: 'IDR', style: 'currency', minimumFractionDigits: 0, maximumFractionDigits: 0};
-    //harga_properti
-    var harga_rumah = parseInt( document.getElementById("harga_rumah").value.replace("Rp", "").replaceAll(".","") );
-    document.getElementById("harga_rumah").value = harga_rumah.toLocaleString('id-ID', Currency);
+    
+    // harga properti
+    var harga_rumah = parseInt( document.getElementById( "harga_rumah" ).value.replace( "Rp", "" ).replaceAll( ".", "" ) );
+    if( !isNaN( harga_rumah ) ) {
+        document.getElementById("harga_rumah").value = harga_rumah.toLocaleString('id-ID', Currency);
+    } else {
+        document.getElementById("harga_rumah").value = "Rp 0";
+    }
     console.log(harga_rumah);
+    
     // suku bunga per tahun
     var suku_bunga = document.getElementById("suku_bunga").value / 100;
+    
+    // jangka waktu tahunan
     var tenor = document.getElementById("tenor").value;
-    var uang_muka = harga_rumah * 20 / 100;
-    document.getElementById("uang_muka").value = uang_muka.toLocaleString('id-ID', Currency);
+    
+    // uang muka
+    var uang_muka = parseInt( document.getElementById( "uang_muka" ).value.replace( "Rp", "" ).replaceAll( ".", "" ) );
+    if( !isNaN( uang_muka ) ) {
+        document.getElementById("uang_muka").value = uang_muka.toLocaleString('id-ID', Currency);
+    } else {
+        document.getElementById("uang_muka").value = "Rp 0";
+        uang_muka = parseInt( document.getElementById( "uang_muka" ).value.replace( "Rp", "" ).replaceAll( ".", "" ) );
+    }
+    // pokok kredit
     var pokok_kredit = harga_rumah - uang_muka;
     document.getElementById("pokok_kredit").value = pokok_kredit.toLocaleString('id-ID', Currency);
-    //angsuran = pokok_kredit * suku_bunga / 12 / (1 - 1 / (1 + suku_bunga / 12)^(-tenor * 12));
+    // angsuran = pokok_kredit * suku_bunga / 12 / (1 - 1 / (1 + suku_bunga / 12)^(-tenor * 12));
     var angsuran = pokok_kredit * suku_bunga / 12 / ( 1 - Math.pow( (1 + suku_bunga / 12), (-tenor * 12) ) );
     document.getElementById("angsuran").value = angsuran.toLocaleString('id-ID', Currency);
     console.log(angsuran);
     console.log(typeof document.getElementById("harga_rumah").value != 'undefined');
 }
-if (typeof document.getElementById("harga_rumah").value != 'undefined' | document.getElementById("harga_rumah").value.includes("Rp")) {
+if ( typeof document.getElementById("harga_rumah").value != 'undefined' | document.getElementById( "harga_rumah" ).value.includes( "Rp" ) ) {
     document.getElementById("harga_rumah").oninput = simulasi_kpr;
+    document.getElementById("uang_muka").oninput = simulasi_kpr;
     document.getElementById("tenor").oninput = simulasi_kpr;
     document.getElementById("suku_bunga").oninput = simulasi_kpr;
 }
